d833bae964a08a8010a957e72d26baa2c9f6992b
mayurpadvi01 <maybeast699@gmail.com>
2024-01-17 11:46:50 +0530

    [ADDED]:mayur - c program addition 2 no.

---
+++ addition.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{ 
+int a,b,c=0;
+
+printf("/n Enter the A value");
+scanf("%d",&a);
+
+printf("/n Enter the B value");
+scanf("%d",&b);
+
+c=a+b;
+
+printf("The result of opretion:-%d",c);
+
+return 0;
+			
+			
+}
